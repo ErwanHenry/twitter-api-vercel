63d514dec22d6dced2f7b846789be30da0307518
ErwanHenry <erwanhenry@hotmail.com>
2025-08-09 14:33:26 +0200

    Fix auth to use API_SECRET_TOKEN instead of KCT_API_BEARER

--- api/_utils/auth.js
+++ api/_utils/auth.js
@@ -1,5 +1,5 @@
 export function checkAuth(req, res) {
-  if (req.headers.authorization !== `Bearer ${process.env.KCT_API_BEARER}`) {
+  if (req.headers.authorization !== `Bearer ${process.env.API_SECRET_TOKEN}`) {
     res.status(401).json({ error: "unauthorized" });
     return false;
   }
